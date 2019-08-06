d371acc411e0261470c3c840f31b32c11046e5cf
KarlLichterVonRandoll <286631670@qq.com>
2019-08-06 16:52:56 +0800

    2019.08.05

--- month03/blog/js/blog.js
+++ month03/blog/js/blog.js
@@ -53,4 +53,101 @@ function checkvalue(){
 
     }
 }
-// 绑定按钮的单机事件
+
+// 定义一个基于列表页的业务逻辑
+var lst={
+    template: function(t,u,p1,p2){
+        var _html = "";
+        _html += '<div class="item">';
+        _html += '<div class="title">';
+        _html += '<h3>'+t+'</h3>';
+        _html += '</div>';
+        _html += '<div class="con">';
+        _html += '<div class="cleft">';
+        _html += '<img src="'+u+'" alt="">';
+        _html += '</div>';
+        _html += '<div class="cright">';
+        _html += '<p class="ptop">'+p1+'</p>';
+        _html += '<p class="pbottom">'+p2+'</p>';
+        _html += '</div>';
+        _html += '</div></div>';
+        return _html;
+    }
+}
+// 使用数组保存数据
+var arrData = [
+    {
+        t: "Python语言优势",
+        u: "imgs/d.jpg",
+        p1: "本文探讨了Python语言在AI领域的优势和运用。谁会成为AI 和大数据时代的第一开发语言？",
+        p2: "皮皮下 学无止境 2019-8-23 3457已阅读 99",
+    },
+    {
+        t: "Java语言优势",
+        u: "imgs/c.jpg",
+        p1: "本文探讨了JAVA语言在AI领域的优势和运用。谁会成为AI 和大数据时代的第一开发语言？",
+        p2: "皮皮下 学无止境 2019-8-23 3457已阅读 99",
+    },
+    {
+        t: "C++语言优势",
+        u: "imgs/e.jpg",
+        p1: "本文探讨了C++语言在AI领域的优势和运用。谁会成为AI 和大数据时代的第一开发语言？",
+        p2: "皮皮下 学无止境 2019-8-23 3457已阅读 99",
+    }
+]
+// 使用流程
+// 1.遍历数组，获取每一项元素对象
+// 2.将获取的元素对象填充到模板中
+// 3.向页面元素追加模板
+for (var i=0;i<arrData.length;i++){
+    var _HTML = lst.template(arrData[i].t,
+                             arrData[i].u,
+                             arrData[i].p1,
+                             arrData[i].p2);
+    $(".lst").prepend(_HTML);
+}
+
+// 定义一个基于图片页的业务逻辑对象
+var pics={
+    template: function(u,n,t){
+        _html = "";
+        _html += '<div class="item">';
+        _html += '<div class="imgs">';
+        _html += '<img src="'+u+'" alt="">';
+        _html += '<div class="tip">喜欢 | '+n+'</div>';
+        _html += '</div>';
+        _html += '<div class="title">';
+        _html += '<h3>'+t+'</h3>';
+        _html += '</div>';
+        _html += '</div>';
+        return _html;
+    }
+}
+// 使用数组保存数据
+var arrPics = [
+    {
+        t: "Python基础学习总结(一)",
+        u: "imgs/toppic02.jpg",
+        n: "199",
+    },
+    {
+        t: "Sublime text3不能正常使用",
+        u: "imgs/toppic01.jpg",
+        n: "199",
+    },
+    {
+        t: "Python学习网址及笔记",
+        u: "imgs/banner01.jpg",
+        n: "199",
+    }
+]
+// 使用流程
+// 1.遍历数组，获取每一项元素对象
+// 2.将获取的元素对象填充到模板中
+// 3.向页面元素追加模板
+for (var j=0;j<arrPics.length;j++){
+    var _HTML = pics.template(arrPics[j].u,
+                             arrPics[j].n,
+                             arrPics[j].t,);
+    $("#pics").append(_HTML);
+}
